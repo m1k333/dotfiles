53e2097a8ec20392027e0cb9b61eb2f9e46f6a68
Michael Richer <msricher1993@gmail.com>
2015-05-04 19:53:40 -0400

    condensed .emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -20,7 +20,6 @@
 
 ;;; StumpWM swank sever
 (require 'stumpwm-mode)
-(require 'stumpwm-utils)
 (setq stumpwm-shell-program "~/.stumpwm.d/modules/util/stumpish/stumpish")
 
 ;;;; Set up Emacs
@@ -234,6 +233,51 @@ default font is available."
   (interactive)
   (stumpwm-mode -1)
   (slime-disconnect))
+
+(defun stumpwm-daemon-shutdown-emacs (&optional display)
+  " This is a function that can bu used to shutdown save buffers and
+shutdown the emacs daemon. It should be called using
+emacsclient -e '(stumpwm-daemon-shutdown-emacs)'.  This function will
+check to see if there are any modified buffers or active clients
+or frame.  If so an x window will be opened and the user will
+be prompted."
+  (let (new-frame modified-buffers active-clients-or-frames)
+    ; Check if there are modified buffers or active clients or frames.
+    (setq modified-buffers (modified-buffers-exist))
+    (setq active-clients-or-frames ( or (> (length server-clients) 1)
+                                        (> (length (frame-list)) 1)))
+    ; Create a new frame if prompts are needed.
+    (when (or modified-buffers active-clients-or-frames)
+      (when (not (eq window-system 'x))
+        (message "Initializing x windows system.")
+        (x-initialize-window-system))
+      (when (not display) (setq display (getenv "DISPLAY")))
+      (message "Opening frame on display: %s" display)
+      (select-frame (make-frame-on-display display '((window-system . x)))))
+    ; Save the current frame.
+    (setq new-frame (selected-frame))
+    ; When displaying the number of clients and frames:
+    ; subtract 1 from the clients for this client.
+    ; subtract 2 from the frames this frame (that we just created) and the default frame.
+    (when ( or (not active-clients-or-frames)
+               (yes-or-no-p (format "There are currently %d clients and %d frames. Exit anyway?"
+                                    (- (length server-clients) 1) (- (length (frame-list)) 2))))
+      ; If the user quits during the save dialog then don't exit emacs.
+      ; Still close the terminal though.
+      (let((inhibit-quit t))
+             ; Save buffers
+        (with-local-quit
+          (save-some-buffers))
+        (if quit-flag
+          (setq quit-flag nil)
+          ; Kill all remaining clients
+          (progn
+            (dolist (client server-clients)
+              (server-delete-client client))
+                 ; Exit emacs
+            (kill-emacs)))))
+    ; If we made a frame then kill it.
+    (when (or modified-buffers active-clients-or-frames) (delete-frame new-frame))))
 
 (defun sudo-edit (&optional arg)
   "Edit a file as root."
